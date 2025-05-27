6213ab95c8e458c63ab349a8c70f080029ba9f79
irfnhanif <14.irfanh@gmail.com>
2025-05-27 16:44:22 +0700

    feat: implement routing in App component using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './pages/Homepage'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import HomePage from "./pages/Homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
-      <HomePage></HomePage>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
