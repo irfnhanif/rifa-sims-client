7a7a816122b4860dff6a7012e9bacde1449d97d6
irfnhanif <14.irfanh@gmail.com>
2025-05-28 15:52:30 +0700

    feat: add ItemListPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import HomePage from "./pages/Homepage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ItemListPage from "./pages/ItemListPage";
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
+          <Route path="/items" element={<ItemListPage />}></Route>
         </Routes>
       </Router>
     </>
