b541132f73e7ddb4a3bd9adc913608504d4ead92
irfnhanif <14.irfanh@gmail.com>
2025-06-05 11:56:51 +0700

    feat: add User interface and request types for user management

---
+++ src/types/user.ts
@@ -0,0 +1,23 @@
+import type { UserRole } from "./user-role"
+import type { UserType } from "./user-type"
+
+export interface User {
+    id: string
+    username: string
+    password: string
+    branch: number
+    role: UserRole
+    status: UserType
+    addedToNotification: boolean
+}
+
+export interface RegisterRequest {
+  username: string;
+  password: string;
+  branch: number;
+}
+
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
